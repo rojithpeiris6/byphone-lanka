2a73a5ffad0e69e0139be71d6649867a84b8591f
Rojith Peiris <rojithonline@gmail.com>
2026-06-01 11:12:48 +0530

    netlift config

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,11 @@
 import { defineConfig } from "vite";
 import { tanstackStart } from "@tanstack/react-start/plugin/vite";
-import { cloudflare } from "@cloudflare/vite-plugin";
 import react from "@vitejs/plugin-react";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
   plugins: [
     tsconfigPaths(),
-    cloudflare({ viteEnvironment: { name: "ssr" } }),
     tanstackStart(),
     react(),
   ],
