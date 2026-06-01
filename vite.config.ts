173f46d4309703018421c070d956775bbe09babd
Rojith Peiris <rojithonline@gmail.com>
2026-06-01 12:05:29 +0530

    fix: add tailwindcss plugin to vite config for production build

--- vite.config.ts
+++ vite.config.ts
@@ -3,10 +3,12 @@ import { tanstackStart } from "@tanstack/react-start/plugin/vite";
 import netlify from "@netlify/vite-plugin-tanstack-start";
 import react from "@vitejs/plugin-react";
 import tsconfigPaths from "vite-tsconfig-paths";
+import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
   plugins: [
     tsconfigPaths(),
+    tailwindcss(),
     tanstackStart(),
     netlify(),
     react(),
