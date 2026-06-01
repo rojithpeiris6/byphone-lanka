c6cdeaacb73e660ac4fe17b4839b065ddbba7557
Rojith Peiris <rojithonline@gmail.com>
2026-06-01 10:47:04 +0530

    s

--- vite.config.ts
+++ vite.config.ts
@@ -2,12 +2,11 @@ import { defineConfig } from "vite";
 import { tanstackStart } from "@tanstack/react-start/plugin/vite";
 import { cloudflare } from "@cloudflare/vite-plugin";
 import react from "@vitejs/plugin-react";
-// 1. මේ ලයින් එක අලුතින් ඇතුළත් කරන්න 
 import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
   plugins: [
-    tsconfigPaths(), // 2. ප්ලගින් ලිස්ට් එකේ උඩින්ම මේක දාන්න
+    tsconfigPaths(),
     cloudflare({ viteEnvironment: { name: "ssr" } }),
     tanstackStart(),
     react(),
