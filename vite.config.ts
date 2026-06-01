5d723f6cd618edc20794c9b06a6206aa6a475dc1
Rojith Peiris <rojithonline@gmail.com>
2026-06-01 11:46:03 +0530

    netlify config

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,14 @@
 import { defineConfig } from "vite";
 import { tanstackStart } from "@tanstack/react-start/plugin/vite";
+import netlify from "@netlify/vite-plugin-tanstack-start";
 import react from "@vitejs/plugin-react";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
   plugins: [
     tsconfigPaths(),
-    tanstackStart(), // 👈 අර අවුල් සහගත server කෑල්ල සම්පූර්ණයෙන්ම අයින් කළා
+    tanstackStart(),
+    netlify(),
     react(),
   ],
 });
